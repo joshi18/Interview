b30b40d9c6129015237d6fa759e682d91625f377
joshi18 <abhijit.joshi0310@gmail.com>
2025-08-14 20:58:49 +0530

    "Creation Design Patterns"

--- src/DesignPatterns/CreationalDesignPatterns/FactoryDesign/ProfessionFactory.java
+++ src/DesignPatterns/CreationalDesignPatterns/FactoryDesign/ProfessionFactory.java
@@ -6,6 +6,7 @@ public class ProfessionFactory {
 
         if (typeofProfession == null){
             return null;
+
         }
         if (typeofProfession.equalsIgnoreCase("Doctor")){
             return new Doctor();
