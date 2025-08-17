0c53119575909fc57784a8923b52a98663ac1aec
joshi18 <abhijit.joshi0310@gmail.com>
2025-08-17 16:45:11 +0530

    "Stream basic to advanced"

--- src/streamapi/MediumAndAdvancedQuestions.java
+++ src/streamapi/MediumAndAdvancedQuestions.java
@@ -3,6 +3,28 @@ package streamapi;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
+//🔹 Medium-Level Stream Questions
+//
+//Find the second largest number in a list.
+//
+//Find the first non-repeating character in a string.
+//
+//Group words by their length.
+//
+//Find the most frequent element in a list.
+//
+//Count the number of distinct elements in a list.
+//
+//Reverse each word in a list while keeping the order of words intact.
+//
+//Find all duplicate elements in a list.
+//
+//Merge two lists into one without duplicates.
+//
+//        Check if a string is an anagram of another string using Streams.
+
+//Find the average length of words in a list.
 public class MediumAndAdvancedQuestions {
     public static void main(String[] args) {
         List<Integer> abc = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 9);
@@ -11,6 +33,7 @@ public class MediumAndAdvancedQuestions {
         List<String> groupofString = Arrays.asList("Abhijit", "Joshi", "alskwih", "hsdfr", "lkjhg", "lkcod");
         List<Integer> k = Arrays.asList(3, 3, 4, 5, 6, 7, 3, 2, 2, 3, 4, 5, 6, 7, 78, 8, 8, 88, 90);
         List<String> q6 = Arrays.asList("Abhijit", "Joshi", "ZWES", "rtyuio");
+        List<Integer> q7 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 9);
 
         // Q1
         Optional<Integer> c = abc.stream().sorted((a, b) -> b - a).distinct().skip(1).findFirst();
@@ -71,6 +94,14 @@ public class MediumAndAdvancedQuestions {
 
 
         // Q7
+        List<Integer> listofIntegers = q7.stream()
+                .collect(Collectors.toMap(a -> a, v -> 1, (w, t) -> w + t))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() > 1)
+                .map(a -> a.getKey())
+                .collect(Collectors.toList());
+        System.out.println(listofIntegers);
 
 
     }
