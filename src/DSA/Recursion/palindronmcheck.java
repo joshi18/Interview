fe785aac5398e8ba60d50c6a1759eba1af92d0ed
joshi18 <abhijit.joshi0310@gmail.com>
2026-03-07 10:28:33 +0530

    "Interview Prepartion"

--- src/DSA/Recursion/palindronmcheck.java
+++ src/DSA/Recursion/palindronmcheck.java
@@ -1,25 +1,24 @@
-package DSA.Recursion;
+package src.DSA.Recursion;
 
 public class palindronmcheck {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String abc = "abcbaaaa";
         int i = 0;
         int j = abc.length();
-        System.out.println(checkpalindome(abc,i,j-1));
+        System.out.println(checkpalindome(abc, i, j - 1));
 
     }
 
-    public static boolean checkpalindome(String a,int i,int j){
-        if (j==0){
+    public static boolean checkpalindome(String a, int i, int j) {
+        if (j == 0) {
             return true;
         }
-        if (i==j){
+        if (i == j) {
             return a.charAt(i) == a.charAt(j);
         }
 
-        return a.charAt(i)== a.charAt(j) && checkpalindome(a,i+1,j-1);
-
+        return a.charAt(i) == a.charAt(j) && checkpalindome(a, i + 1, j - 1);
 
 
     }
